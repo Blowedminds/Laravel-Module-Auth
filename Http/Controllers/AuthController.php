ffc9fb552f6dd8a2fbef07a8849d7fa0c737b3da
blowedminds <ruzgardeniz.08@hotmail.com>
2018-07-14 13:24:46 +0300

    reset password added

--- Http/Controllers/AuthController.php
+++ Http/Controllers/AuthController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Modules\Auth\Http\Controllers;
 
 use App\Modules\Core\Role;
@@ -9,95 +10,116 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth:api', ['only' => ['logout', 'register']]);
-  }
-
-  public function login(Request $request)
-  {
-    $this->validate($request, [
-        'email' => 'required|email',
-        'password' => 'required'
-    ]);
-
-    $credentials = $request->only('email', 'password');
-
-    $login = self::_login($credentials);
-
-    return response()->json($login['data'], $login['status']);
-  }
-
-  public function logout()
-  {
-    app('auth')->invalidate(app('auth')->getToken());
-
-    return response()->json(true, 200);
-  }
-
-  public function register()
-  {
-    $messages = [
-      'name.required' => 'İsim gerekli, lütfen isminizi giriniz',
-      'email.required' => 'E-posta adresi gerekli, lütfen e-posta adresi giriniz',
-      'email.email' => 'E-posta adresiniz geçersiz, lütfen geçerli bir e-posta adresi giriniz',
-      'email.unique' => 'Bu e-posta adresi zaten kullanılıyor',
-      'password.required' => 'Şifre gerekli, lütfen şifrenizi giriniz',
-      'password.min' => 'Şifreniz en az 6 karakterli olmalı',
-      'password.confirmed' => 'Şifreleriniz uyuşmuyor, kontrol ediniz',
-      'password_confirmation.required' => 'Şifre tekrarı gerekli'
-    ];
-
-    request()->validate([
-        'name' => 'required',
-        'email' => 'required|email|unique:users',
-        'password' => 'required|min:6|confirmed',
-        'password_confirmation' => 'required',
-        'role_id' => 'required'
-    ], $messages);
-
-    $unique_id = uniqid('user');
-
-    $user = new User([
-        'user_id' => $unique_id,
-        'name' => request()->input('name'),
-        'email' => request()->input('email'),
-        'password' => app('hash')->make(request()->input('password')),
-    ]);
-
-    $user->save();
-
-    $role = Role::findOrFail(request()->input('role_id'));
-
-    UserData::create([
-      'user_id' => $user->user_id,
-      'role_id' => $role->id,
-      'profile_image' => 'DEFAULT_IMAGE',
-      'biography' => ['tr' => 'Buraya kendinizi anlatan kısa bir metin yazın!']
-    ]);
-
-    $login = self::_login(request()->only('email', 'password'));
-
-    return response()->json($login['data'], $login['status']);
-  }
-
-  public function isAuthenticated()
-  {
-      return response()->json( (bool) auth()->user(), 200);
-  }
-
-  private static function _login($credentials)
-  {
-    try{
-        if (!$token = app('auth')->attempt($credentials)){
-
-            return ['data' => ['error' => 'Invalid Credentials'], 'status' => 401];
-        }
-    }catch(JWTException $e) {
-
-        return ['data' => ['error' => 'Could not create a token'], 'status' => 500];
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['only' => ['logout', 'register', 'resetPassword']]);
     }
 
-    return ['data' => ['token' => $token], 'status' => 200];
-  }
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        $login = self::_login($credentials);
+
+        return response()->json($login['data'], $login['status']);
+    }
+
+    public function logout()
+    {
+        app('auth')->invalidate(app('auth')->getToken());
+
+        return response()->json(true, 200);
+    }
+
+    public function register()
+    {
+        $messages = [
+            'name.required' => 'İsim gerekli, lütfen isminizi giriniz',
+            'email.required' => 'E-posta adresi gerekli, lütfen e-posta adresi giriniz',
+            'email.email' => 'E-posta adresiniz geçersiz, lütfen geçerli bir e-posta adresi giriniz',
+            'email.unique' => 'Bu e-posta adresi zaten kullanılıyor',
+            'password.required' => 'Şifre gerekli, lütfen şifrenizi giriniz',
+            'password.min' => 'Şifreniz en az 6 karakterli olmalı',
+            'password.confirmed' => 'Şifreleriniz uyuşmuyor, kontrol ediniz',
+            'password_confirmation.required' => 'Şifre tekrarı gerekli'
+        ];
+
+        request()->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+            'password_confirmation' => 'required',
+            'role_id' => 'required'
+        ], $messages);
+
+        $unique_id = uniqid('user');
+
+        $user = new User([
+            'user_id' => $unique_id,
+            'name' => request()->input('name'),
+            'email' => request()->input('email'),
+            'password' => app('hash')->make(request()->input('password')),
+        ]);
+
+        $user->save();
+
+        $role = Role::findOrFail(request()->input('role_id'));
+
+        UserData::create([
+            'user_id' => $user->user_id,
+            'role_id' => $role->id,
+            'profile_image' => 'DEFAULT_IMAGE',
+            'biography' => ['tr' => 'Buraya kendinizi anlatan kısa bir metin yazın!']
+        ]);
+
+        $login = self::_login(request()->only('email', 'password'));
+
+        return response()->json($login['data'], $login['status']);
+    }
+
+    public function resetPassword()
+    {
+        request()->validate([
+            'password' => 'required|min:6|confirmed',
+            'password_confirmation' => 'required',
+        ]);
+        
+        $user = auth()->user();
+
+        $user->password = app('hash')->make(request()->input('password'));
+
+        $user->save();
+
+        return response()->json([
+           'header' => 'İşlem Başarılı',
+           'message' => 'Parola başarılı bir şekilde güncellendi',
+           'action' => 'Tamam',
+           'state' => 'success'
+        ]);
+    }
+
+    public function isAuthenticated()
+    {
+        return response()->json((bool)auth()->user(), 200);
+    }
+
+    private static function _login($credentials)
+    {
+        try {
+            if (!$token = app('auth')->attempt($credentials)) {
+
+                return ['data' => ['error' => 'Invalid Credentials'], 'status' => 401];
+            }
+        } catch (JWTException $e) {
+
+            return ['data' => ['error' => 'Could not create a token'], 'status' => 500];
+        }
+
+        return ['data' => ['token' => $token], 'status' => 200];
+    }
 }
--- Http/auth.php
+++ Http/auth.php
@@ -4,6 +4,8 @@ Route::put('register', 'AuthController@register');
 
 Route::post('login', 'AuthController@login');
 
+Route::post('reset-password', 'AuthController@resetPassword');
+
 Route::get('logout', 'AuthController@logout');
 
 Route::get('is-authenticated', 'AuthController@isAuthenticated');
